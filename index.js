91747a9588a10bc156c6cd396ac3b629bdd3ee95
marcioartes <marcioarteletras@gmail.com>
2025-02-10 00:30:12 -0300

    fix vercel.json

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/src", express.static(path.join(__dirname, "src")));
-app.get("/", (req, res) => {
+
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
